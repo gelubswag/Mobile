fafaf8fc8294843fc039ee1084e0a7f56095b4af
gelubswag <vlaznevigor@yandex.ru>
2025-09-14 19:10:20 +0300

    Core

---
+++ Core/Task3/Solution.java
@@ -0,0 +1,34 @@
+/*
+Кесарю — кесарево
+*/
+public class Solution {
+    public static void main(String[] args) {
+        Pet pet1 = new Cat();
+        Pet cat = pet1.getChild();
+        System.out.println(cat);
+
+        Pet pet2 = new Dog();
+        Pet dog = pet2.getChild();
+        System.out.println(dog);
+    }
+
+    public static class Pet {
+        public Pet getChild() {
+            return new Pet();
+        }
+    }
+
+    public static class Cat extends Pet {
+        @Override
+        public Cat getChild() {
+            return new Cat();
+        }
+    }
+
+    public static class Dog extends Pet {
+        @Override
+        public Dog getChild() {
+            return new Dog();
+        }
+    }
+}
---
+++ Core/Task4/Solution.java
@@ -0,0 +1,19 @@
+/* 
+Int и Integer
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        print(2);
+        print(Integer.valueOf(2));
+    }
+
+    //Напишите тут ваши методы
+    public static void print(int h) {
+        System.out.print(h);
+    }
+
+    public static void print(Integer f) {
+        System.out.print(f);
+    }
+}
